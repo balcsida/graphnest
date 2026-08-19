31a558bb76379c245930c6e2c62de1e77f1cc147
Dávid Balatoni <balcsida@gmail.com>
2026-08-19 12:16:04 +0200

    test(images): exclude scanner from application image

--- deploy/images/test.sh
+++ deploy/images/test.sh
@@ -15,6 +15,7 @@ docker run --rm --read-only --tmpfs /tmp --tmpfs /var/run/grepnest \
     command -v grepnest-admin
     command -v grepnest-migrate
     command -v grepnest-mcp
+    ! command -v grepnest-scanner >/dev/null
     command -v wget
   '
 
